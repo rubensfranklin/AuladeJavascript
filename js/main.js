dfd0fdee1055a85ca9c4c7c4efbc990cc767e5a3
Franklin <47985472+rubensfranklin@users.noreply.github.com>
2021-02-08 14:15:51 -0300

    Atualização
    Atualização

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+//var nome = "Franklin Santos"
+//alert("Bem Vindo! sou " + nome)
+
+
+var idade = prompt("Qual sua Idade?");
+
+if(idade >= 18)
+{
+    alert("Maior de Idade!")
+}
+else{
+    alert("Menor de idade")
+}
